f2e6ae0777a565bcdb05f6459bfec895341d4774
Daniel Reynolds <danielreynolds101@gmail.com>
2021-09-12 19:19:12 -0600

    make types, import socketio

--- frontend-controller/src/App.tsx
+++ frontend-controller/src/App.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Grid } from '@material-ui/core'
+
+type ControlTernary = -1 | 0 | 1;
+interface ControlState {
+    fw: ControlTernary,
+    lr: ControlTernary
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Grid container>
+                <Grid item xs={12} md>
+
+                </Grid>
+                <Grid item>
+
+                </Grid>
+            </Grid>
+        </div>
+    );
 }
 
 export default App;
